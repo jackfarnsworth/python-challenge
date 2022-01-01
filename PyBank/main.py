1adff7bcfb44a28761a38406263e0beac4884823
jackcfarnsworth <jackcfarnsworth@gmail.com>
2022-01-01 12:50:14 -0500

    fixed error caused by caluculating total months and made array of changes in profit/loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,5 +6,17 @@ filepath = os.path.join("Resources", "budget_data.csv")
 with open(filepath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
-    totalmonths = len(list(csvreader))
-    print(totalmonths)
+    total = 0
+    totalmonths = 0
+    previous = 0
+    changes = []
+    for row in csvreader:
+        totalmonths += 1
+        changes.append(int(row[1] - previous))
+    
+
+    
+
+
+
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,4 +6,3 @@ filepath = os.path.join("Resources", "election_data.csv")
 with open(filepath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
-    
